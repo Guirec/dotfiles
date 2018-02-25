9e5ab6826d769c7cf9afb66540241f70dc6c444a
Guirec Lefort <guirec.lefort@gmail.com>
2018-02-25 20:45:15 +0100

    Update GPG Suite caskroom identifier

--- macos/apps/work.sh
+++ macos/apps/work.sh
@@ -9,7 +9,7 @@ print_in_purple "\n   Working tools\n\n"
 
 # ---------------------------------------------------------------------
 
-brew_install "GPG Suite" "gpgtools" "caskroom/cask" "cask"
+brew_install "GPG Suite" "gpg-suite" "caskroom/cask" "cask"
 brew_install "Harvest" "harvest" "caskroom/cask" "cask"
 brew_install "Skype" "skype" "caskroom/cask" "cask"
 brew_install "Slack" "slack" "caskroom/cask" "cask"
