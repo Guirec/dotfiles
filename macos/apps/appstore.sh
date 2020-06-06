ed656070867eac7a9455fe5e5f600622ac6c82a6
Guirec Lefort <guirec@lefort.family>
2020-06-06 03:05:59 +0200

    Simplify Xcode installation

--- macos/apps/appstore.sh
+++ macos/apps/appstore.sh
@@ -9,6 +9,7 @@ print_in_purple "\n   App Store\n"
 
 # ---------------------------------------------------------------------
 
+mas_install "Paste" "967805235"
 mas_install "Spark" "1176895641"
 mas_install "Tweetbot" "1384080005"
-mas_install "Paste" "967805235"
+mas_install "Xcode" "497799835"
