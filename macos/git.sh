c23a77d4bd3dc709081cfc2fddf47435d28527a0
Guirec Lefort <guirec@lefort.family>
2020-06-08 11:01:17 +0200

    Add Tig and lazygit tools for Git

--- macos/git.sh
+++ macos/git.sh
@@ -11,3 +11,5 @@ print_in_purple "\n • Git\n\n"
 
 brew_install "Git" "git"
 brew_install "GitHub CLI" "github/gh/gh"
+brew_install "lazygit" "jesseduffield/lazygit/lazygit"
+brew_install "Tig" "tig"
