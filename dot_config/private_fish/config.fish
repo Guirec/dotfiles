5b949e0f1040cbd4a609ccb3c19a62796a67c404
Guirec Lefort <bonjour@guirec.dev>
2024-03-16 19:01:02 +0100

    Update fish config and theme

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,10 +3,8 @@ function fish_greeting; end
 function fish_right_prompt; end
 function fish_mode_prompt; end
 
-set GPG_TTY (tty)
-
 # Environment
-set EDITOR vscode
+set EDITOR zed
 
 # Vi Bindings
 set fish_cursor_default     block
@@ -14,9 +12,21 @@ set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
 
+# Alias
+alias ll="eza -l -g --icons --git"
+alias lla="eza -l -g -a --icons --git"
+alias llt="eza -1 --icons --tree --git-ignore"
+alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim"
+
+# Abbreviations
+abbr lg 'lazygit'
+
 # Source
-source ~/.config/fish/theme_tokyonight_storm.fish
-source ~/.config/fish/functions.fish
+# source ~/.config/fish/themes/catppuccin_mocha.theme
+source ~/.config/fish/functions/sudo_last_command.fish
+
+# Zoxide
+zoxide init fish | source
 
 # Starship prompt
 starship init fish | source
